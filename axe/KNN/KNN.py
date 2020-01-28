6bd4603ba0806eb1c3042f4ee2dc1e7c366b1053
JKChang2015 <jkchang2015@gmail.com>
2020-01-28 10:39:17 +0000

    update

--- axe/KNN/KNN.py
+++ axe/KNN/KNN.py
@@ -4,6 +4,8 @@
 # Tag:
 # Description: 
 
+import operator
+
 import matplotlib.pyplot as plt
 from numpy import *
 
@@ -23,9 +25,33 @@ def drawGraph(group, labels):
     plt.show()
 
 
-def classify0(inX, deataSet, lables, k):
-    pass
+def classify(newInput, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    ## step 1: calculate Euclidean distance
+    # tile(A, reps): Construct an array by repeating A reps times
+    # the following copy numSamples rows for dataSet
+    diffMat = tile(newInput, (dataSetSize, 1))- dataSet # Subtract element-wise
+    sqDiffMat = diffMat ** 2  # squared for the subtract
+    sqDistances = sqDiffMat.sum(axis=1) # sum is performed by row
+    distances = sqDistances ** 0.5
+
+    ## step 2: sort the distance
+    # argsort() returns the indices that would sort an array in a ascending order
+    sortedDistIndicies = distances.argsort()
+
+    classCount = {}
+    for i in range(k):
+        ## step 3: choose the min k distance
+        voteIlabel = labels[sortedDistIndicies[i]]
+        ## step 4: count the times labels occur
+        # when the key voteLabel is not in dictionary classCount, get() will return 0
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    ## step 5: the max voted class will return
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
 group, labels = createDataSet()
 drawGraph(group, labels)
+print(classify([0.5, 0.5], group, labels, 2))
---
+++ axe/README.md
@@ -0,0 +1,3 @@
+# Todo
+---
+* convert MNIST training and test sets to 32 bit binary code.
